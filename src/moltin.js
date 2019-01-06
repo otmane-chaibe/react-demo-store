9a436d3ff2432bd4cb4115967861ba525ea9ee8d
Otmane Chaibe <oc35@njit.edu>
2019-01-05 22:42:48 -0500

    Update moltin.js

--- src/moltin.js
+++ src/moltin.js
@@ -1,6 +1,6 @@
 const MoltinGateway = require('@moltin/sdk').gateway;
 
-let client_id = 'j6hSilXRQfxKohTndUuVrErLcSJWP15P347L6Im0M4';
+let client_id = 'vpDFE9uA3bKMIQRGEQEiPJTfZPaHFklFDy0OtdOmz';
 
 if (process.env.REACT_APP_MOLTIN_CLIENT_ID) {
   client_id = process.env.REACT_APP_MOLTIN_CLIENT_ID;
